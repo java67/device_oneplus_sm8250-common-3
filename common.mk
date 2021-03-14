573d499079dee25f346fb9aca50f1176140bf53c
blankaf <realblankaf@gmail.com>
2021-03-14 13:10:09 +0100

    sm8250-common: Import powerhint from LA.UM.9.12.r1-10800-SMxx50.0

--- common.mk
+++ common.mk
@@ -399,6 +399,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
